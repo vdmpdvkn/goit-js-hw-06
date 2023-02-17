471f867275749e70f58238a5dc52764d14436daf
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-02-17 20:52:15 +0300

    look task 8 comment on 5 line pls.

--- js/task-08.js
+++ js/task-08.js
@@ -2,7 +2,7 @@ const loginFormEl = document.querySelector(".login-form");
 console.log(loginFormEl);
 loginFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  const { email, password } = e.currentTarget;
+  const { email, password } = e.currentTarget; // не понял зачем в примере из конспекта использовался elements: {e, p}, выполнил обычную деструктуризацию, работает точно также. Если это ошибка, или плохой тон, напишите пожалуйста какую роль играет elements в этом случае :)
   if (email.value === "" || password.value === "") {
     alert("Введите валидные данные");
   } else {
